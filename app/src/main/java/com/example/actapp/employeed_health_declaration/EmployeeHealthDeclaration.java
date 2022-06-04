c1f32d3cea9c8ad559cd51dd96aa6b1db08aac96
dyawbot <jawarapaglinawan@gmail.com>
2022-06-04 19:51:24 +0800

    GuestEmployeeUpdate

--- app/src/main/java/com/example/actapp/employeed_health_declaration/EmployeeHealthDeclaration.java
+++ app/src/main/java/com/example/actapp/employeed_health_declaration/EmployeeHealthDeclaration.java
@@ -4,30 +4,85 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.actapp.R;
 import com.example.actapp.employee_success_declaration.EmployeeSuccessDeclaration;
+import com.example.actapp.guest_visit.GuestAboutVisit;
 
 public class EmployeeHealthDeclaration extends AppCompatActivity {
 
     Button e_healthDeclaration;
+    boolean fromGuest = false;
+    Intent intents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_health_declaration);
 
+
         e_healthDeclaration = findViewById(R.id.e_healthDeclaration);
+        //e_healthDeclaration.setOnClickListener((View.OnClickListener) this);
+
+
+
+        Bundle extras = getIntent().getExtras();
+        String userName;
+
+        if (extras != null) {
+            userName = extras.getString("fromGuest");
+            // and get whatever type user account id is
+            Log.i("HELLO", String.valueOf(userName));
+
+            if(userName.equals("guest"))
+            {
+
+
+                fromGuest = true;
+
+
+
+            }
+            else
+            {
+                fromGuest = false;
+            }
+
+        }
+
+
 
 
         e_healthDeclaration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), EmployeeSuccessDeclaration.class);
-                startActivity(intent);
+
+
+                if(fromGuest == true)
+                {
+                    Intent intent = new Intent(getApplicationContext(), GuestAboutVisit.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    Intent intent = new Intent(getApplicationContext(), EmployeeSuccessDeclaration.class);
+                    startActivity(intent);
+                }
+
             }
         });
+
+
+
+
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/actapp/guest_personal_info/GuestPersonalInformation.java
+++ app/src/main/java/com/example/actapp/guest_personal_info/GuestPersonalInformation.java
@@ -2,15 +2,36 @@ package com.example.actapp.guest_personal_info;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.actapp.R;
+import com.example.actapp.employeed_health_declaration.EmployeeHealthDeclaration;
+
+import android.view.View;
+import android.widget.Button;
 
 public class GuestPersonalInformation extends AppCompatActivity {
+    
+    
+    Button b_guest_personal_information;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest_personal_information);
+
+
+        b_guest_personal_information = findViewById(R.id.b_guest_personal_information);
+
+
+        b_guest_personal_information.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), EmployeeHealthDeclaration.class);
+                intent.putExtra("fromGuest", "guest");
+                startActivity(intent);
+            }
+        });
     }
 }
